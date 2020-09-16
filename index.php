905acb929c0cd1a36373dbdfc55337733f6fccf3
amir seyfi <amirseyfi0221@gmail.com>
2020-09-16 06:57:58 -0700

    add photos in div informations

--- index.php
+++ index.php
@@ -126,81 +126,99 @@
 
     <div class="infoo">
         <div class="card informations bg text-center collapse mb-3 ml-auto " style="max-width: 22rem;" id="winner">
-            <div class="card-header bgc"> آسود </div>
-            <div class="card-body bg">
+            <div class="card-header bg">
+                <div class="card-body bgc"> آسود </div>
                 <p class="card-text">
                     تبریک میگوییم
                     شما برنده شده اید برای دریافت جایزه
                     کد زیر را در پروفایل خود وارد کنید
                 </p>
                 <hr>
+                <img class="circlephoto" src="photos/circle.png" alt="circle">
+                <img class="arrowphoto" src="photos/arrow.png" alt="arrow">
                 <h4 id="code">  </h4>
             </div>
         </div>
         <div class="card informations bg text-center collapse mb-3 ml-auto  ml-auto mb-auto" style="max-width: 22rem;" data-info="earth">
-            <div class="card-header bgc"> زمین </div>
-            <div class="card-body bg">
+            <div class="card-header bg">
+                <div class="card-body bgc"> زمین </div>
                 <p class="card-text">
                     زمین، تنها سیاره در منظومه شمسی است که آب می‌تواند در هر سه حالت جامد، مایع و گاز در آن وجود داشته باشد.
 یک حقیقت شگفت‌انگیز  در مورد زمین، این است که 99 درصد از تمام گونه‌های مختلفی که تا به حال روی زمین وجود داشته‌اند، منقرض شده‌اند.
                 </p>
             </div>
+            <img class="circlephoto" src="photos/circle.png" alt="circle">
+            <img class="arrowphoto" src="photos/arrow.png" alt="arrow">
         </div>
         <div class="card informations bg text-center collapse mb-3 ml-auto " style="max-width: 22rem;" data-info="mars">
-            <div class="card-header bgc"> مریخ </div>
-            <div class="card-body bg">
+            <div class="card-header bg">
+                <div class="card-body bgc"> مریخ </div>
                 <p class="card-text">
                     اگر در مریخ باشید، آسمان شما دیگر آبی نخواهد بود! بلکه شما یک آسمان صورتی رنگ خواهید داشت. هر وقت به سرتان زد به مریخ سفر کنید، لباس گرم را فراموش نکنید. دمای متوسط مریخ حدود 60- درجه سانتی‌گراد است، دمایی حدود دمای قطب جنوب ( البته بدون حضور پنگوئن‌ها )
                  </p>
+                 <img class="circlephoto" src="photos/circle.png" alt="circle">
+                 <img class="arrowphoto" src="photos/arrow.png" alt="arrow">
             </div>
         </div>
         <div class="card informations bg collapse text-center mb-3 ml-auto " style="max-width: 22rem;" data-info="saturn">
-            <div class="card-header bgc"> زحل </div>
-            <div class="card-body bg">
+            <div class="card-header bg">
+                <div class="card-body bgc"> زحل </div>
                 <p class="card-text">
                     نکته جالب در مورد سیاره زحل این است که این سیاره می‌تواند روی آب شناور بماند.
                 </p>
             </div>
+            <img class="circlephoto" src="photos/circle.png" alt="circle">
+            <img class="arrowphoto" src="photos/arrow.png" alt="arrow">
         </div>
         <div class="card informations bg text-center collapse mb-3 ml-auto " style="max-width: 22rem;" data-info="venus">
-            <div class="card-header bgc"> زهره </div>
-            <div class="card-body bg">
+            <div class="card-header bg">
+                <div class="card-body bgc"> زهره </div>
                 <p class="card-text">
                     نکته جالب در مورد سیاره زهره این است که سال این سیاره کوتاه‌تر از شبانه روزش است و خورشید در سیاره زهره از مغرب طلوع می‌کند.
                 </p>
             </div>
+            <img class="circlephoto" src="photos/circle.png" alt="circle">
+            <img class="arrowphoto" src="photos/arrow.png" alt="arrow">
         </div>
         <div class="card informations bg text-center collapse mb-3 ml-auto " style="max-width: 22rem;" data-info="uranus">
-            <div class="card-header bgc"> اورانوس </div>
-            <div class="card-body bg">
+            <div class="card-header bg">
+                <div class="card-body bgc"> اورانوس </div>
                 <p class="card-text">نکته جالب در مورد سیاره اورانوس این است که شما نمیتوانید روی اورانوس بایستید، هسته‌ی جامد و سنگی دارد اما توپ بزرگی است که بخش اصلی آن از گازهلی هیدروژن وهلیوم تشکیل شده‌است.
     ابرهای اورانوس از سولفید هیدروژن تشکیل شده‌اند، یعنی بویی شبیه تخم‌مرغ گندیده می‌دهند.
                 </p>
             </div>
+            <img class="circlephoto" src="photos/circle.png" alt="circle">
+            <img class="arrowphoto" src="photos/arrow.png" alt="arrow">>
         </div>
         <div class="card informations bg text-center collapse mb-3 ml-auto " style="max-width: 22rem;" data-info="jupiter">
-            <div class="card-header bgc"> مشتری </div>
-            <div class="card-body bg">
+            <div class="card-header bg">
+                <div class="card-body bgc"> مشتری </div>
                 <p class="card-text">بزرگ‌ترین قمر سیاره‌ی مشتری اقیانوسی نمکی دارد که آب آن از آب‌های موجود بر کره‌ی زمین بیشتر است.
                 تعطیلات در ساحل مدیترانه را فراموش کنید.اگر می‌خواهید مناظر اقیانوس بیکران را ببینید. این قمر مشتری حتی بزرگتر از سیاره عطارد است و اگر به جای مشتری به دور خورشید می‌چرخد، می‌توانست به تنهایی یک سیاره حساب شود.
                 </p>
             </div>
+            <img class="circlephoto" src="photos/circle.png" alt="circle">
+            <img class="arrowphoto" src="photos/arrow.png" alt="arrow">
         </div>
         <div class="card informations bg text-center collapse mb-3 ml-auto " style="max-width: 22rem;" data-info="neptune">
-            <div class="card-header bgc"> نپتون </div>
-            <div class="card-body bg">
+            <div class="card-header bg">
+                <div class="card-body bgc"> نپتون </div>
                 <p class="card-text">
                     نپتون به معنای خداوند دریا می‌باشد و از نام اساطیر رومیان باستان می‌باشد. این سیاره نخستین سیاره‌‌ای است که به واسطه پیش‌بینی‌های فضایی ثبت شده‌است. سال‌ها پیش " الکسیس بووارد" فضانورد، متوجه اختلافاتی در مدار اورانوس شد و حدس زد که این سیاره در نزدیکی سیاره‌ای دیگر سکونت دارد.
                 </p>
             </div>
+            <img class="circlephoto" src="photos/circle.png" alt="circle">
+            <img class="arrowphoto" src="photos/arrow.png" alt="arrow">
         </div>
         <div class="card informations bg text-center collapse mb-3 ml-auto " style="max-width: 22rem;" data-info="mercury">
-            <div class="card-header bgc"> عطارد </div>
-            <div class="card-body bg">
+            <div class="card-header bg">
+                <div class="card-body bgc"> عطارد </div>
                 <p class="card-text">
                     در مدت سه ماه می‌توانید کتابی بنویسید یا برای امتحان رانندگی آماده و قبول شوید یا دوره مقدماتی آموزش گیتار را بگذرانید. این همان مدت زمانی است که عطارد به دور خورشید می‌گردد. این کوتاه‌ترین مدار منظومه شمسی است چون عطارد نزدیک‌ترین  سیاره به خورشید است. پس اگر این بار کسی از شما پرسید کی کاری را قول دادید انجام می‌دهید بگویید حدود یک سال عطاردی دیگر
                 </p>
             </div>
+            <img class="circlephoto" src="photos/circle.png" alt="circle">
+            <img class="arrowphoto" src="photos/arrow.png" alt="arrow">
         </div>
     </div>
 
